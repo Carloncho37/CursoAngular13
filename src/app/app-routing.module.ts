becd021226d8f4a0ed634bd5f38f6ef600a33bfa
Carlos Galvan <carlos.galvan@ute-salta.com.ar>
2024-03-25 09:32:11 -0300

    fin seccion 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Error404pageComponent } from './shared/pages/error404page/error404page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: ()  => import('./auth/auth.module').then  (m => m.AuthModule),
+  },
+  {
+    path: 'heroes',
+    loadChildren: ()  => import('./heroes/heroes.module').then  (m => m.HeroesModule),
+  },
+  {
+    path: '404',
+    component: Error404pageComponent,
+  },
+    //? Defino que la ruta vacia exacta, rediriga a heroes
+    //? Si no uso patMatch full, todas las rutas caerian en 'heroes'
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { Error404pageComponent } from './pages/error404page/error404page.component';
+import { NgModule } from '@angular/core';
+
+
+
+@NgModule({
+  declarations: [
+    Error404pageComponent
+  ],
+  exports: [
+    Error404pageComponent
+  ]
+})
+export class SharedModule { }
